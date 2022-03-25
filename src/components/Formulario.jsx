461bd5882c84af84baff10b8954762787fc556f5
Luis <lmendozag.lmmg@gmail.com>
2022-03-25 12:18:03 -0600

    Clase 72

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,14 +1,46 @@
+import { useState } from 'react'
+
 export const Formulario = () => {
+  const [nombre, setNombre] = useState('')
+  const [propietario, setPropietario] = useState('')
+  const [email, setEmail] = useState('')
+  const [fecha, setFecha] = useState('')
+  const [sintomas, setSintomas] = useState('')
+
+  const [error, setError] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    // validación del formulario
+    if ([nombre, propietario, email, fecha, sintomas].includes('')) {
+      console.log('Hay un campo vacio')
+
+      setError(true)
+      return
+    }
+
+    setError(false)
+  }
+
   return (
-    <div className='w-1/2 lg:w-3/5'>
+    <div className='w-1/2 lg:w-3/5 mx-5'>
       <h2 className='font-black text-3xl text-center'>Seguimiento Pacientes</h2>
 
       <p className='text-lg mt-5 text-center mb-10'>
         Añade Pacientes y{' '}
         <span className='text-indigo-600 font-bold'>Administralos</span>
       </p>
+      <form className='bg-white shadow-md rounded py-10 px-5 mb-10' onSubmit={handleSubmit}>
 
-      <form className='bg-white shadow-md rounded py-10 px-5 mb-10'>
+        {error && (
+          <div className=' bg-red-500 text-white text-center p-3 font-bold mb-3 rounded-md'>
+            <p>
+              Todos los campos son obligaroios
+            </p>
+          </div>
+        )}
+
         <div className=' mb-5'>
           <label
             htmlFor='mascota'
@@ -21,6 +53,8 @@ export const Formulario = () => {
             id='mascota'
             placeholder='Nombre de la mascota'
             className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
           />
         </div>
         <div className=' mb-5'>
@@ -35,6 +69,8 @@ export const Formulario = () => {
             id='propietario'
             placeholder='Nombre del propietario'
             className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+            value={propietario}
+            onChange={(e) => setPropietario(e.target.value)}
           />
         </div>
 
@@ -50,6 +86,8 @@ export const Formulario = () => {
             id='email'
             placeholder='Email Contacto Propietario'
             className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
 
@@ -64,6 +102,8 @@ export const Formulario = () => {
             type='date'
             id='alta'
             className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+            value={fecha}
+            onChange={(e) => setFecha(e.target.value)}
           />
         </div>
         <div className=' mb-5'>
@@ -73,7 +113,11 @@ export const Formulario = () => {
           >
             Síntomas
           </label>
-          <textarea id='sintomas' placeholder='Describe los síntomas' className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md' />
+          <textarea
+            id='sintomas' placeholder='Describe los síntomas' className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+            value={sintomas}
+            onChange={(e) => setSintomas(e.target.value)}
+          />
         </div>
 
         <input type='submit' value='Agregar Paciente' className='w-full p-3 rounded bg-indigo-600 text-white uppercase-md cursor-pointer font-bold hover:bg-indigo-700 transition-colors' />
---
+++ src/components/Paciente.jsx
@@ -0,0 +1,27 @@
+
+export const Paciente = () => {
+  return (
+    <div className=' mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl'>
+      <p className=' font-bold mb-3 text-gray-700 uppercase'>
+        Nombre: {' '}
+        <span className=' font-normal normal-case'>Hook</span>
+      </p>
+      <p className=' font-bold mb-3 text-gray-700 uppercase'>
+        Propietario: {' '}
+        <span className=' font-normal normal-case'>Luis</span>
+      </p>
+      <p className=' font-bold mb-3 text-gray-700 uppercase'>
+        Email: {' '}
+        <span className=' font-normal normal-case'>correo@correo.com</span>
+      </p>
+      <p className=' font-bold mb-3 text-gray-700 uppercase'>
+        Fecha Alta: {' '}
+        <span className=' font-normal normal-case'>10 Diciembre de 2022</span>
+      </p>
+      <p className=' font-bold mb-3 text-gray-700 uppercase'>
+        Síntomas: {' '}
+        <span className=' font-normal normal-case'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis laudantium consectetur ea voluptate sunt repellat architecto minus blanditiis inventore modi odio dolorum culpa, suscipit voluptatum at, odit corporis obcaecati qui.</span>
+      </p>
+    </div>
+  )
+}
