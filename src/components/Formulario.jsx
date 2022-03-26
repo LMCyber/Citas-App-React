f3db136c82bdc483f88aab620f88f4fac5747c5e
Luis <lmendozag.lmmg@gmail.com>
2022-03-26 00:23:10 -0600

    Fin Proyecto

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { Error } from './Error'
 
-export const Formulario = () => {
+export const Formulario = ({ pacientes, setPacientes, paciente, setPaciente }) => {
   const [nombre, setNombre] = useState('')
   const [propietario, setPropietario] = useState('')
   const [email, setEmail] = useState('')
@@ -8,6 +9,23 @@ export const Formulario = () => {
   const [sintomas, setSintomas] = useState('')
 
   const [error, setError] = useState(false)
+
+  useEffect(() => {
+    if (Object.keys(paciente).length > 0) {
+      setNombre(paciente.nombre)
+      setPropietario(paciente.propietario)
+      setEmail(paciente.email)
+      setFecha(paciente.fecha)
+      setSintomas(paciente.sintomas)
+    }
+  }, [paciente])
+
+  const generarId = () => {
+    const random = Math.random().toString(36).substring(2)
+    const fecha = Date.now().toString(36)
+
+    return random + fecha
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -21,6 +39,37 @@ export const Formulario = () => {
     }
 
     setError(false)
+
+    // Objeto del paciente
+    const objetoPaciente = {
+      nombre,
+      propietario,
+      email,
+      fecha,
+      sintomas
+    }
+
+    if (paciente.id) {
+      // Editando el registro
+      objetoPaciente.id = paciente.id
+
+      const pacientesActualizados = pacientes.map(pacienteState => pacienteState.id === paciente.id ? objetoPaciente : pacienteState)
+
+      setPacientes(pacientesActualizados)
+      setPaciente({})
+    } else {
+      // Nuevo Registro
+      objetoPaciente.id = generarId()
+
+      setPacientes([...pacientes, objetoPaciente])
+    }
+
+    // Reiniciar Form
+    setNombre('')
+    setPropietario('')
+    setEmail('')
+    setFecha('')
+    setSintomas('')
   }
 
   return (
@@ -31,14 +80,14 @@ export const Formulario = () => {
         Añade Pacientes y{' '}
         <span className='text-indigo-600 font-bold'>Administralos</span>
       </p>
-      <form className='bg-white shadow-md rounded py-10 px-5 mb-10' onSubmit={handleSubmit}>
-
+      <form
+        className='bg-white shadow-md rounded py-10 px-5 mb-10'
+        onSubmit={handleSubmit}
+      >
         {error && (
-          <div className=' bg-red-500 text-white text-center p-3 font-bold mb-3 rounded-md'>
-            <p>
-              Todos los campos son obligaroios
-            </p>
-          </div>
+          <Error>
+            <p>Todos los campos son obligatorios</p>
+          </Error>
         )}
 
         <div className=' mb-5'>
@@ -114,13 +163,19 @@ export const Formulario = () => {
             Síntomas
           </label>
           <textarea
-            id='sintomas' placeholder='Describe los síntomas' className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+            id='sintomas'
+            placeholder='Describe los síntomas'
+            className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
             value={sintomas}
             onChange={(e) => setSintomas(e.target.value)}
           />
         </div>
 
-        <input type='submit' value='Agregar Paciente' className='w-full p-3 rounded bg-indigo-600 text-white uppercase-md cursor-pointer font-bold hover:bg-indigo-700 transition-colors' />
+        <input
+          type='submit'
+          value={paciente.id ? 'Editar Paciente' : 'Agregar Paciente'}
+          className='w-full p-3 rounded bg-indigo-600 text-white uppercase-md cursor-pointer font-bold hover:bg-indigo-700 transition-colors'
+        />
       </form>
     </div>
   )
--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,23 +1,47 @@
+
 import { Paciente } from './Paciente'
 
-export const ListadoPacientes = () => {
+export const ListadoPacientes = ({ pacientes, setPaciente, eliminarPaciente }) => {
   return (
     <div className='md:w-1/2 lg:w-3/5 md:h-screen overflow-scroll'>
 
-      <h2 className='font-black text-3xl text-center'>
-        Listado Pacientes
-      </h2>
+      {pacientes && pacientes.length
+        ? (
+          <>
+            <h2 className='font-black text-3xl text-center'>
+              Listado Pacientes
+            </h2>
 
-      <p className=' text-xl mt-5 mb-10 text-center'>
-        Administra tus {' '}
-        <span className=' text-indigo-600 font-bold'>
-          pacientes y citas
-        </span>
-      </p>
+            <p className=' text-xl mt-5 mb-10 text-center'>
+              Administra tus {' '}
+              <span className=' text-indigo-600 font-bold'>
+                pacientes y citas
+              </span>
+            </p>
+          </>
+          )
+        : (
+          <>
+            <h2 className='font-black text-3xl text-center'>
+              No hay pacientes
+            </h2>
 
-      <Paciente />
-      <Paciente />
-      <Paciente />
+            <p className=' text-xl mt-5 mb-10 text-center'>
+              Comienza agregando pacientes {' '}
+              <span className=' text-indigo-600 font-bold'>
+                y aparecerán en este lugar
+              </span>
+            </p>
+          </>
+          )}
+
+      {pacientes.map((paciente) => (
+        <Paciente
+          key={paciente.id}
+          paciente={paciente}
+          setPaciente={setPaciente}
+          eliminarPaciente={eliminarPaciente}
+        />))}
 
     </div>
   )
